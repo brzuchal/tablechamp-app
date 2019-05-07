2a3158565779ac8b9f457ecd63e993e87778bde8
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2019-05-07 11:47:56 +0200

    Added firebase apiKey

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyCVy3uKW-lX7pxLK2bfCNjXOgeWHLhuFz0', 
+  "authDomain": 'tf.brzuchal.com', 
+  "databaseURL": 'https://table-football-d9b8b.firebaseio.com' 
 };
